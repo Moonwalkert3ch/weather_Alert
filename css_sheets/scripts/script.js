9448ad374963bfd0fb0cbbfae92efecc8ee8277f
Moonwalkert3ch <6620@holbertonstudents.com>
2023-12-15 19:46:04 -0600

    working on dropdown js

--- css_sheets/scripts/script.js
+++ css_sheets/scripts/script.js
@@ -6,8 +6,13 @@ function searchByZip() {
 
 function toggleDropdown() {
     const dropdown = document.getElementById("dropdown");
-    dropdown.style.display = (dropdown.style.display === "block") ? "none" : "block";
+    if (dropdown.style.display === "block") {
+        dropdown.style.display = "none";
+    } else {
+        dropdown.style.display = "block";
+    }
 }
+   
 
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(event) {
