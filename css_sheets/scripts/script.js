52e30a00289528bb1fda96c1f711fafc1b9a8606
Moonwalkert3ch <6620@holbertonstudents.com>
2023-12-15 18:09:25 -0600

    added zipcode field box and searchbutton

---
+++ css_sheets/scripts/script.js
@@ -0,0 +1,5 @@
+function searchByZip() {
+    var zipCode = document.getElementById("zipcode").value;
+    // Add your logic for handling the ZIP code search here
+    alert("Searching for ZIP code: " + zipCode);
+}
