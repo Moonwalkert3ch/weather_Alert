b84f24ce27ca0ff65ec86eea64ea2688bb0b7f96
Moonwalkert3ch <6620@holbertonstudents.com>
2023-12-15 19:40:56 -0600

    adjusted location of gear icon

--- css_sheets/scripts/script.js
+++ css_sheets/scripts/script.js
@@ -3,3 +3,18 @@ function searchByZip() {
     // Add your logic for handling the ZIP code search here
     alert("Searching for ZIP code: " + zipCode);
 }
+
+function toggleDropdown() {
+    var dropdown = document.getElementById("dropdown");
+    dropdown.style.display = (dropdown.style.display === "block") ? "none" : "block";
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.gear-container')) {
+        var dropdown = document.getElementById("dropdown");
+        if (dropdown.style.display === "block") {
+            dropdown.style.display = "none";
+        }
+    }
+}
