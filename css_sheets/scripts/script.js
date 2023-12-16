3b78afad62b3480f526cbdcbfe39bc2e4c020719
Moonwalkert3ch <6620@holbertonstudents.com>
2023-12-15 19:58:18 -0600

    update menu dropdown still not displaying though

--- css_sheets/scripts/script.js
+++ css_sheets/scripts/script.js
@@ -1,12 +1,14 @@
 function searchByZip() {
-    var zipCode = document.getElementById("zipcode").value;
+    const zipCode = document.getElementById("zipcode").value;
     // Add your logic for handling the ZIP code search here
     alert("Searching for ZIP code: " + zipCode);
 }
 
 function toggleDropdown() {
     const dropdown = document.getElementById("dropdown");
-    if (dropdown.style.display === "block") {
+    const computedStyle = window.getComputedStyle(dropdown);
+
+    if (computedStyle.display === "block") {
         dropdown.style.display = "none";
     } else {
         dropdown.style.display = "block";
@@ -15,10 +17,12 @@ function toggleDropdown() {
    
 
 // Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
+window.onclick = function (event) {
     if (!event.target.matches('.gear-container')) {
         const dropdown = document.getElementById("dropdown");
-        if (dropdown.style.display === "block") {
+        const computedStyle = window.getComputedStyle(dropdown);
+
+        if (computedStyle.display === "block") {
             dropdown.style.display = "none";
         }
     }
