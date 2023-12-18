b0778d7a69436a952d57bcc518d4bdab7c5d5ab7
Blythe Lockhart <blythe.lockhart@att.net>
2023-12-18 06:12:16 -0600

    updated scripts to allow for dropdown

--- css_sheets/scripts/script.js
+++ css_sheets/scripts/script.js
@@ -4,32 +4,19 @@ function searchByZip() {
     alert("Searching for ZIP code: " + zipCode);
 }
 
-.dropdown {
-    display: none;
-}
-
-.show-dropdown {
-    display: block;
-}
-
 function toggleDropdown() {
     const dropdown = document.getElementById("dropdown");
-    dropdown.classlist.toggle("show-description");
+    const computedStyle = window.getComputedStyle(dropdown);
+
+    if (computedStyle.display === "none") {
+        dropdown.style.display = "block";
+    } else {
+        dropdown.style.display = "none";
+    }
 }
    
 
 // Close the dropdown if the user clicks outside of it
-// window.onclick = function (event) {
-//     if (!event.target.matches('.gear-container')) {
-//         const dropdown = document.getElementById("dropdown");
-//         const computedStyle = window.getComputedStyle(dropdown);
-//
-//         if (computedStyle.display === "block") {
-//             dropdown.style.display = "none";
-//         }
-//     }
-// };
-
 document.addEventListener("click", function (event) {
     const dropdown = document.getElementById("dropdown");
     if (!event.target.closest('.gear-container') && !event.target.closest('.dropdown')) {
